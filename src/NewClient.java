913d775324b0e2c402f9e8c54a9b5ec5415bc598
4yvakZ <vanek009ki@gmail.com>
2019-04-18 21:26:24 +0300

    Serializable Human in NewClient

--- src/NewClient.java
+++ src/NewClient.java
@@ -6,10 +6,10 @@ import people.Fool;
 import people.Human;
 import rocket.room.Room;
 
-import java.io.*;
-import java.io.FileNotFoundException;
+import java.io.ByteArrayOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
@@ -17,10 +17,9 @@ import java.net.PortUnreachableException;
 import java.nio.CharBuffer;
 import java.util.Scanner;
 
-import static java.lang.System.out;
 
 public class NewClient {
-    private Room room;
+    private static Room room;
     public static void main(String[] args) throws IOException {
         System.out.println("Welcome to Client side");
         DatagramSocket socket = new DatagramSocket();
@@ -36,23 +35,46 @@ public class NewClient {
                 String line = scanner.nextLine();
                 if (line.equalsIgnoreCase("disconnect")) break;
                 if (line.isEmpty()) continue;
+                String firstWord = line.split(" ")[0];
                 try {
-                    if (line.split(" ", 2)[0].equals("import")){
+                    if (firstWord.equals("import")){
                         line = "load "+ readFile(line.split(" ", 2)[1]);
                     }
                 }catch (IOException e) {
                     System.out.println("File " + line.split( " ", 2)[1] + " does not exist");
                     continue;
                 }
-                buf = line.getBytes();
-                DatagramPacket packet = new DatagramPacket(buf, buf.length);
-                socket.send(packet);
-
+                Human human = null;
+                if(firstWord.equals("add")|| firstWord.equals("remove_lower")|| firstWord.equals("add_if_max")|| firstWord.equals("remove")){
+                    try {
+                        human = readJSON(line.split(" ", 2)[1]);
+                        buf = firstWord.getBytes();
+                        DatagramPacket packet = new DatagramPacket(buf, buf.length);
+                        socket.send(packet);
+                        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+                        ObjectOutputStream objos = new ObjectOutputStream(bos);
+                        objos.writeObject(human);
+                        objos.close();
+                        buf = bos.toByteArray();
+                        packet = new DatagramPacket(buf, buf.length);
+                        socket.send(packet);
+                    } catch (ParseException e) {
+                        System.out.println("Wrong format!");
+                        continue;
+                    } catch (NullPointerException e){
+                        System.out.println("wrong format!");
+                        continue;
+                    }
+                }else {
+                    buf = line.getBytes();
+                    DatagramPacket packet = new DatagramPacket(buf, buf.length);
+                    socket.send(packet);
+                }
                 byte[] buffer = new byte[65000];
                 DatagramPacket packet1 = new DatagramPacket(buffer, buffer.length);
                 socket.receive(packet1);
                 received = new String(packet1.getData(), 0, packet1.getLength());
-                out.println(received);
+                System.out.println(received);
             }
             socket.close();
         }catch (PortUnreachableException e){
@@ -123,7 +145,7 @@ public class NewClient {
         System.out.println(msg);
         return data;
     }
-    private Human readJSON(String string) throws ParseException, NullPointerException {
+    private static Human readJSON(String string) throws ParseException, NullPointerException {
         Human human;
         JSONObject jo = (JSONObject) new JSONParser().parse(string);
         String name;
